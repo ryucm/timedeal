c32a5de8a51b7a22a51e2c8e575baa2aeced5614
ryucm <ckd12394@gmail.com>
2023-02-24 10:53:38 +0900

    Initial Commit

---
+++ src/main/java/com/timedeal/timedeal/TimedealApplication.java
@@ -0,0 +1,13 @@
+package com.timedeal.timedeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimedealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimedealApplication.class, args);
+	}
+
+}
