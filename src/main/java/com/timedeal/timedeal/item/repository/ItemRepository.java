ad35d58bb8df4884b1c695f2a394b20a2bb575e0
류창민 <89899249+ryucm@users.noreply.github.com>
2023-04-06 10:39:45 +0900

    Modify Lock naming in ItemRepository

--- src/main/java/com/timedeal/timedeal/item/repository/ItemRepository.java
+++ src/main/java/com/timedeal/timedeal/item/repository/ItemRepository.java
@@ -1,17 +1,13 @@
 package com.timedeal.timedeal.item.repository;
 
 import com.timedeal.timedeal.item.entity.Item;
-import com.timedeal.timedeal.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.LockModeType;
-import javax.transaction.Transactional;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -20,5 +16,9 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     @Lock(value = LockModeType.PESSIMISTIC_WRITE)
     @Query("select i from Item i where i.itemName = :itemName")
-    Item findByIdWithPessimisticLock(@Param("itemName") String itemName);
+    Item findByItemNameWithPessimisticLock(@Param("itemName") String itemName);
+
+    @Lock(value = LockModeType.OPTIMISTIC)
+    @Query("select i from Item i where i.itemName = :itemName")
+    Item findByItemNameWithOptimisticLock(@Param("itemName") String itemName);
 }
